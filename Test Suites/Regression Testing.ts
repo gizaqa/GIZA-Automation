867cb147bb267a50e1c38603639c05980daef554
gizaqa <adconxqa@gmail.com>
2024-07-22 11:21:37 +0800

    test suite sample

---
+++ Test Suites/Regression Testing.ts
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Regression Testing</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>1dedd023-41f5-48a4-ad48-8e2da9fa432b</testSuiteGuid>
+   <testCaseLink>
+      <guid>881fdb1d-e483-434b-8079-e6a8806f6ace</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/GZA-12286-GH-0424-0974 M2W-C3B1 Incorrect remark display in Check Form for Weld in Flange Plate to Column Flange</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>7c9a5a6b-b31c-4c2a-9fa3-8653eeaae341</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/GZA-12182-GH-0424-0971 S2E-C3 Incorrect remarks display in Check Form</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
